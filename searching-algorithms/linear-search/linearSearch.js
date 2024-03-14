e9913119ac6c5f8dcb9ee59520eec7b40e2e3b2a
Shubham Adelkar <adelkarshubham47@gmail.com>
2024-03-14 15:56:17 +0530

    change in linearSearch exmaple

--- searching-algorithms/linear-search/linearSearch.js
+++ searching-algorithms/linear-search/linearSearch.js
@@ -1,19 +1,19 @@
 // Linear search, using includes() method.
-function search(array, value) {
+function linearSearch(array, value) {
   if (array.length === 0) return null;
   for (let i = 0; i < array.length; i++) {
     return array.includes(value) ? array.indexOf(value) : -1;
   }
 }
 
-console.log(search([1, 2, 3, 4, 5, 6, 7], 4));
+console.log(linearSearch([1, 2, 3, 4, 5, 6, 7], 4));
 
 // Linear search
-function search(array, value) {
+function linearSearch(array, value) {
   if (array.length === 0) return null;
   for (let i = 0; i < array.length; i++) {
     return array[i] === value ? array.indexOf(value) : -1;
   }
 }
 
-console.log(search([1, 2, 3, 4, 5, 6, 7], 4));
+console.log(linearSearch([1, 2, 3, 4, 5, 6, 7], 4));
