fff4eb358f97a7bc5f3f5602dda1bc92757561d7
Shubham Adelkar <adelkarshubham47@gmail.com>
2024-03-14 15:23:36 +0530

    update search

--- searching-algorithms/search.js
+++ searching-algorithms/search.js
@@ -1,9 +1,9 @@
 // Linear search
 function search(array, value) {
-  if (array.length === 0) return 0;
+  if (array.length === 0) return null;
   for (let i = 0; i < array.length; i++) {
-    return array.includes(value) ? true : false;
+    return array.includes(value) ? array.indexOf(value) : -1;
   }
 }
 
-console.log(search([1, 2, 3, 4, 5, 6, 7], 1));
+console.log(search([1, 2, 3, 4, 5, 6, 7], 4));
