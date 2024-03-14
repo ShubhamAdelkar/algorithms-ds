c56d0759eb76f12fb7ab8ab1da959c68b9766a19
Shubham Adelkar <adelkarshubham47@gmail.com>
2024-03-14 11:26:41 +0530

    add exmaple

---
+++ recursion/problem-set/recursiveRange.js
@@ -0,0 +1,10 @@
+/* 
+Write a function called recursiveRange which accepts a number and adds up all the numbers from 0 to the number passed to the function 
+*/
+
+function recursiveRange(number) {
+  if (number === 1) return 1;
+  return number + recursiveRange(number - 1);
+}
+
+console.log(recursiveRange(2));
