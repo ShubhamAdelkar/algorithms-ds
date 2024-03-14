752656d32ebd37b47d3d565922f04e5aff722b2b
Shubham Adelkar <adelkarshubham47@gmail.com>
2024-03-14 13:22:51 +0530

    add fibonacci example

---
+++ recursion/problem-set/fibonacci.js
@@ -0,0 +1,19 @@
+/*
+Write a recursive function called fib which accepts a number and returns the nth number in the Fibonacci sequence. Recall that the Fibonacci sequence is the sequence of whole numbers 1, 1, 2, 3, 5, 8, ... which starts with 1 and 1, and where every number thereafter is equal to the sum of the previous two numbers.
+*/
+
+function fib(number) {
+  if (number === 0) {
+    return 0;
+  }
+  if (number === 1 || number === 2) {
+    return 1;
+  }
+  return fib(number - 1) + fib(number - 2);
+}
+
+console.log(fib(4)); // Output: 3
+console.log(fib(10)); // Output: 55
+console.log(fib(28)); // Output: 317811
+console.log(fib(35)); // Output: 9227465
+
