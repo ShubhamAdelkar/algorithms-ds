c537ada1c17cafe0b536d4d6c1ec86e9c28563f1
Shubham Adelkar <adelkarshubham47@gmail.com>
2024-05-10 13:43:45 +0530

    add quick sort function

--- sorting-algorithms/quick-sort/quick-sort.js
+++ sorting-algorithms/quick-sort/quick-sort.js
@@ -1,20 +1,32 @@
-function pivot(arr, start = 0, end = arr.length - 1) {
+function pivot(array, start = 0, end = array.length - 1) {
   // swap function
   const swap = (array, idx1, idx2) =>
     ([array[idx1], array[idx2]] = [array[idx2], array[idx1]]);
 
-  const pivot = arr[start]; // pivot at start
+  const pivot = array[start]; // pivot at start
   let swapIndex = start; // swapindex at zero
 
   // looping over an array
-  for (let i = start + 1; i < arr.length; i++) {
-    if (pivot > arr[i]) {
+  for (let i = start + 1; i < array.length; i++) {
+    if (pivot > array[i]) {
       swapIndex++;
-      swap(arr, swapIndex, i);
-      //   console.log(arr);
+      swap(array, swapIndex, i);
+      // console.log(array);
     }
   }
+  swap(array, start, swapIndex);
+  // console.log(array);
   return swapIndex;
 }
 
-console.log("index is " + pivot([4, 8, 2, 1, 5, 7, 6, 3]));
+// quick sort implementation
+function QuickSort(array, left = 0, right = array.length - 1) {
+  if (left < right) {
+    let pivotIndex = pivot(array, left, right); // pivot index form pivot()
+    QuickSort(array, left, pivotIndex - 1); // left
+    QuickSort(array, pivotIndex + 1, right); // right
+  }
+  return array;
+}
+
+console.log(QuickSort([1, 34, 5, 6, 4, 3, 55, 7, -2]));
