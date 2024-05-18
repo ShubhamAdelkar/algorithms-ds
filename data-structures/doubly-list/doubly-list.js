59be7863987c4804488c1ee51040cb725ae0d73b
Shubham Adelkar <adelkarshubham47@gmail.com>
2024-05-18 14:13:30 +0530

    update insert method

--- data-structures/doubly-list/doubly-list.js
+++ data-structures/doubly-list/doubly-list.js
@@ -102,18 +102,34 @@ class DoublyLinkedList {
     }
     return false;
   }
+
+  insert(index, val) {
+    if (index < 0 || index > this.length) return false;
+    if (index === 0) return this.unshift(val);
+    if (index === this.length) return this.push(val);
+    let newNode = new Node(val);
+    let beforeNode = this.get(index - 1);
+    let afterNode = beforeNode.next;
+    beforeNode.next = newNode;
+    newNode.prev = beforeNode;
+    newNode.next = afterNode;
+    afterNode.prev = newNode;
+    this.length++;
+    return true;
+  }
 }
 
 let list = new DoublyLinkedList();
 
-list.push(99);
-list.push(100);
-list.push("LAST ITEM");
+list.push("RON");
+list.push("HERMOINE");
+list.push("HARRY");
 list.pop();
 list.shift();
 list.unshift("HELLO");
-list.push("LAST ITEM");
+list.push("HARRY");
 console.log(list);
 list.get(1);
-list.set(0, "HII");
+list.set(0, "TONKS");
+list.insert(1, "NEVILE");
 console.log(list);
