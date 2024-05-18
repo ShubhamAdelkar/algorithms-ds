209935e729ab4cdf0759063515f25c7f2e8bd0f6
Shubham Adelkar <adelkarshubham47@gmail.com>
2024-05-18 10:24:19 +0530

    add push method

---
+++ data-structures/doubly-list/doubly-list.js
@@ -0,0 +1,36 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    let newNode = new Node(val);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
+
+let list = new DoublyLinkedList();
+
+list.push(99);
+list.push(100);
+list.push("LAST ITEM");
+console.log(list);
