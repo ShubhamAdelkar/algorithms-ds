895ad604b164803dab53fb72ea2b346fad97e778
Shubham Adelkar <adelkarshubham47@gmail.com>
2024-05-18 10:48:38 +0530

    add pop method

--- data-structures/doubly-list/doubly-list.js
+++ data-structures/doubly-list/doubly-list.js
@@ -26,6 +26,20 @@ class DoublyLinkedList {
     this.length++;
     return this;
   }
+
+  pop() {
+    if (this.length === 0) return undefined;
+    let poppedNode = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = poppedNode.prev;
+      this.tail.next = null;
+    }
+    this.length--;
+    return this;
+  }
 }
 
 let list = new DoublyLinkedList();
@@ -33,4 +47,6 @@ let list = new DoublyLinkedList();
 list.push(99);
 list.push(100);
 list.push("LAST ITEM");
+console.log(list);
+list.pop();
 console.log(list);
