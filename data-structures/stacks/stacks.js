553a7f00331f4c92b79bc354761afb6a563f3c2d
Shubham Adelkar <adelkarshubham47@gmail.com>
2024-05-19 11:36:36 +0530

    add push method in stack

---
+++ data-structures/stacks/stacks.js
@@ -0,0 +1,32 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      let temp = this.first;
+      this.first = newNode;
+      this.first.next = temp;
+    }
+    this.size++;
+    return this;
+  }
+}
+
+let stack = new Stack();
+stack.push(99);
+console.log(stack);
