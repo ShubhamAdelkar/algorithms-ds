71d8d063b30d20c253288eb1f37964e3275ed6cf
Shubham Adelkar <adelkarshubham47@gmail.com>
2024-05-21 11:33:25 +0530

    add queue & enqueue

---
+++ data-structures/queues/queues.js
@@ -0,0 +1,27 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  enqueue(value) {
+    let newNode = new Node(val);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    this.size++;
+    return this;
+  }
+}
