825d93ee1ced8658a9248da7a665caa4c3e35b73
Shubham Adelkar <adelkarshubham47@gmail.com>
2024-05-23 20:04:49 +0530

    update code

--- data-structures/trees/tree-traversal/BST-BFS.js
+++ data-structures/trees/tree-traversal/BST-BFS.js
@@ -10,47 +10,7 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
-
-  insert(value) {
-    let newNode = new Node(value);
-    if (!this.root) {
-      this.root = newNode;
-      return this;
-    }
-    let current = this.root;
-    while (true) {
-      if (value === current.value) return undefined;
-      if (value < current.value) {
-        if (current.left === null) {
-          current.left = newNode;
-          return this;
-        }
-        current = current.left;
-      } else {
-        if (current.right === null) {
-          current.right = newNode;
-          return this;
-        }
-        current = current.right;
-      }
-    }
-  }
-
-  find(value) {
-    if (!this.root) return false;
-    let current = this.root,
-      found = false;
-    while (current && !found) {
-      if (value < current.value) {
-        current = current.left;
-      } else if (value > current.value) {
-        current = current.right;
-      } else {
-        return true;
-      }
-    }
-    return false;
-  }
+  
   // Breadth-first searching
   BFS() {
     let data = [],
@@ -68,13 +28,4 @@ class BinarySearchTree {
 }
 
 let tree = new BinarySearchTree();
-tree.insert(10);
-tree.insert(12);
-tree.insert(5);
-tree.insert(2);
-tree.insert(3);
-tree.insert(20);
-tree.insert(44);
 console.log(tree.BFS());
-console.log(tree.find(44));
-console.log(tree);
