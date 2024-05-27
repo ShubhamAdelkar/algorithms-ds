3e6d4693b578ca685e401dbfd9ec9c93079e5a86
Shubham Adelkar <adelkarshubham47@gmail.com>
2024-05-27 11:31:20 +0530

    add insert method

---
+++ data-structures/heaps/max-heap.js
@@ -0,0 +1,26 @@
+class MaxBinaryHeap {
+  constructor() {
+    this.values = [55, 44, 33, 22, 11, 4, 2, 1];
+  }
+  insert(element) {
+    this.values.push(element);
+    this.bubbleUp();
+  }
+  bubbleUp() {
+    let idx = this.values.length - 1;
+    const element = this.values[idx];
+    while (idx > 0) {
+      let parentIdx = Math.floor((idx - 1) / 2);
+      let parent = this.values[parentIdx];
+      if (element <= parent) break;
+      this.values[parentIdx] = element;
+      this.values[idx] = parent;
+      idx = parentIdx;
+    }
+  }
+}
+
+let heap = new MaxBinaryHeap();
+heap.insert(56);
+heap.insert(0);
+console.log(heap);
