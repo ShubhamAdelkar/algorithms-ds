3c2dcd923739ac3374461ab334797dfc4b8bb10b
Shubham Adelkar <adelkarshubham47@gmail.com>
2024-05-30 09:22:40 +0530

    add extractMax method

--- data-structures/heaps/max-heap.js
+++ data-structures/heaps/max-heap.js
@@ -1,6 +1,6 @@
 class MaxBinaryHeap {
   constructor() {
-    this.values = [55, 44, 33, 22, 11, 4, 2, 1];
+    this.values = [];
   }
   insert(element) {
     this.values.push(element);
@@ -18,9 +18,55 @@ class MaxBinaryHeap {
       idx = parentIdx;
     }
   }
+
+  extractMax() {
+    const max = this.values[0];
+    const end = this.values.pop();
+    this.values[0] = end;
+    this.sinkDown();
+    return max;
+  }
+  sinkDown() {
+    let idx = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+    while (true) {
+      let leftChildIdx = 2 * idx + 1;
+      let rightChildIdx = 2 * idx + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      if (leftChildIdx < length) {
+        leftChild = this.values[leftChildIdx];
+        if (leftChild > element) {
+          swap = leftChildIdx;
+        }
+      }
+      if (rightChildIdx < length) {
+        rightChild = this.values[rightChildIdx];
+        if (
+          (swap === null && rightChild > element) ||
+          (swap !== null && rightChild > leftChild)
+        ) {
+          swap = rightChildIdx;
+        }
+      }
+      if (swap === null) break;
+      this.values[idx] = this.values[swap];
+      this.values[swap] = element;
+      idx = swap;
+    }
+  }
 }
 
 let heap = new MaxBinaryHeap();
-heap.insert(56);
-heap.insert(0);
+heap.insert(41);
+heap.insert(39);
+heap.insert(33);
+heap.insert(18);
+heap.insert(27);
+heap.insert(12);
+heap.insert(55);
+heap.extractMax();
+heap.extractMax();
 console.log(heap);
