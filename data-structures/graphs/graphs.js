949f74c32493f8801de183518eeff83d1dbfa762
Shubham Adelkar <adelkarshubham47@gmail.com>
2024-06-03 12:51:06 +0530

    add remove edge

--- data-structures/graphs/graphs.js
+++ data-structures/graphs/graphs.js
@@ -10,7 +10,21 @@ class Graph {
   }
 
   addEdge(v1, v2) {
+    this.addVertex(v1);
+    this.addVertex(v2);
     this.adjencyList[v1].push(v2);
     this.adjencyList[v2].push(v1);
   }
+
+  removeEdge(v1, v2) {
+    this.adjencyList[v1] = this.adjencyList[v1].filter((v) => v !== v2);
+    this.adjencyList[v2] = this.adjencyList[v2].filter((v) => v !== v1);
+  }
 }
+
+let graph = new Graph();
+graph.addEdge("Dallas", "Tokyo");
+graph.addEdge("New York", "London");
+console.log(graph);
+graph.removeEdge("New York", "London");
+console.log(graph);
